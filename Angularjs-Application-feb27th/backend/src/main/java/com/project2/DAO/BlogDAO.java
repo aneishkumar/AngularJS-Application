9a69f81f21694b16a9aaeb85fe6d42f18e9e5d9a
aneish <aneish>
2018-02-27 17:10:26 +0530

    This is my commit

---
+++ Angularjs-Application-feb27th/backend/src/main/java/com/project2/DAO/BlogDAO.java
@@ -0,0 +1,12 @@
+package com.project2.DAO;
+
+import java.util.List;
+
+import com.project2.model.Blog;
+
+public interface BlogDAO {
+	public void addBlog(Blog blog);
+	public List<Blog> getallblog();
+	
+
+}
