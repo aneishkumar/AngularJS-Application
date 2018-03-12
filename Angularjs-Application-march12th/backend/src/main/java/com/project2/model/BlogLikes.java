3c062308dddb584751652f4fa40814f689d51462
aneish <aneish>
2018-03-12 15:15:49 +0530

    This is my commit

---
+++ Angularjs-Application-march12th/backend/src/main/java/com/project2/model/BlogLikes.java
@@ -0,0 +1,19 @@
+package com.project2.model;
+
+public class BlogLikes {
+
+	int blogid;
+	public int getBlogid() {
+		return blogid;
+	}
+	public void setBlogid(int blogid) {
+		this.blogid = blogid;
+	}
+	public int getLikes() {
+		return likes;
+	}
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	int likes;
+}
---
+++ Angularjs-Application-march12th/backend/src/main/java/com/project2/model/Error.java
@@ -0,0 +1,27 @@
+package com.project2.model;
+
+public class Error {
+	private int code;
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	private String message;
+	public Error(int code,String message)
+	{
+		super();
+		this.code = code;
+		this.message = message;
+	}
+	
+		
+
+}
